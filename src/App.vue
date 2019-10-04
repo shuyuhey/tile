b117da69b2524301d74eff241624341b17eb7cf0
Shuhei Takasugi <shuhei.takasugi@gmail.com>
2019-10-04 15:59:34 +0900

    タイルのサンプル

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="addPicture">Add Picture</button>
+
+    <PictureContainer>
+      <LiquidContainer v-for="picture, index in pictures" :index="index" :length="pictures.length">
+        <RemovableContainer>
+          <Picture>
+            <img :src="picture" alt="">
+          </Picture>
+        </RemovableContainer>
+      </LiquidContainer>
+    </PictureContainer>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import styled from 'vue-styled-components';
+import faker from "faker";
+
+const RemovableContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  &::before {
+    content: "☓";
+    background: white;
+    font-weight: bold;
+    position: absolute;
+    top: 4px;
+    right: 4px;
+  }
+`;
+
+const LiquidContainer = styled('div', { index: Number, length: Number })`
+  ${props => props.length == 1 && 'width: 100%;'}
+  ${props => props.length > 1 && 'width: 50%;'}
+
+  height: calc(var(--max-width) / 2 - 16px);
+  ${props => props.length !== 1 && props.length % 2 == 1 && props.index > (props.length - 3) && 'height: calc(var(--max-width) / 4 - 8px);'}
+
+  min-width: 50%;
+  float: left;
+`;
+
+const Picture = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: gray;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const Container = styled.div`
+  &::after {
+    clear: both;
+  }
+`;
 
 @Component({
   components: {
-    HelloWorld
+    PictureContainer: Container,
+    Picture,
+    RemovableContainer,
+    LiquidContainer
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private pictures: string[] = [];
+
+
+  addPicture(): void {
+    const image = faker.image.avatar();
+
+    this.pictures.push(image);
+  }
+}
 </script>
 
 <style>
+  :root {
+    --max-width: 375px;
+  }
 #app {
+  max-width: 375px;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto 0;
+  padding: 16px;
+  height: 100vh;
 }
 </style>
